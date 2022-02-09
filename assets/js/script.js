5b101ebf0541ac92ba45916b8a522b2e5eba6018
chloë js <chloejs.horta@gmail.com>
2022-02-09 13:19:47 +0000

    js clean up running function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+// We need a function to return a random number in range 1 to 5 for a computer choice.
 function getComputerChoice() {
     return Math.floor((Math.random() * 5) + 1);
 }
 
+//We want to add some consts that would help us later.
 const ROCK = 'ROCK';
 const SCISSORS = 'SCISSORS';
 const PAPER = 'PAPER';
@@ -22,23 +24,30 @@ const OPTIONS2EMOJI = {
     5: '🦎'
 };
 
+// Now we want to store user input somewhere.
 let userChoiceID = '';
 
+//We want to add event listeners for user input when a page is loaded. 
+//To do that we should write our code inside the callback function.
 window.onload = () => {
-    // your code here
+    //We need an element to output results. 
     const output = document.querySelector('#output');
-
+    // We want  to add event listeners to handle user input.
+    // First, we would select elements by class. Then we would add an event listener to each element. 
     const userOptions = document.querySelectorAll('.user-option');
 
     userOptions.forEach(el => el.addEventListener('click', event => {
         userChoiceID = event.target.id;
     }));
-}
 
+
+//Now we want to add an event listener to the play button.
 const playButton = document.querySelector('#play');
+// event listener
 playButton.addEventListener('click', () => {
 
-
+// information that we need to determine the winner.
+//We need a computer and user choice. 
     const computerChoiceID = getComputerChoice();
     const computerChoice = OPTIONS[computerChoiceID];
     const computerChoiceElement = document.querySelector('#computer-choice');
@@ -46,8 +55,11 @@ playButton.addEventListener('click', () => {
     const userChoice = OPTIONS[userChoiceID];
 
     let result = '';
+//Now we can output computer choice.
     computerChoiceElement.innerHTML = OPTIONS2EMOJI[computerChoiceID];
 
+//Let's write logic to determine a winner. We would use a switch for that.
+// Don't forget to add a default case.(?)
     switch (`${computerChoice}-${userChoice}`) {
         case `${ROCK}-${ROCK}`:
         case `${SCISSORS}-${SCISSORS}`:
@@ -82,8 +94,11 @@ playButton.addEventListener('click', () => {
             break;
         default:
             result = 'SOMETHING WRONG. TRY AGAIN. 🐛'
-
+        }
+//Now we are ready to output the result.
             output.innerHTML = result;
-            console.log(results)
-    }
-})
+    })
+//Put it at the end of the play button click callback.
+}
+//hodovani ##
+//https://hackernoon.com/how-to-create-rock-paper-scissors-spock-lizard-in-javascript-991k36hy//
